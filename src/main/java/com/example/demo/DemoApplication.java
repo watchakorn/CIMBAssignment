467d969d24aade9f6b0a77960e43adbcac27b526
Watchakorn Wongtibet <w.wongtibet@gmail.com>
2024-03-11 20:27:28 +0700

    add step 2 find best profit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		DemoApplication demo = new DemoApplication();
-		Long[] req = { 7l, 1l, 5l, 3l, 6l, 4l };
+//		 Long[] req = { 7L, 1L, 5L, 3L, 6L, 4L };
+		Long[] req = { 7L, 6L, 5L, 4L, 3L, 1L };
 		demo.q1(req);
 	}
 
@@ -17,17 +18,32 @@ public class DemoApplication {
 			}
 			System.out.println("Price quantity: " + prices.length);
 
-			// 1. find day to buy.
+			// 1. find day to buy and price.
 			Long price = prices[0];
-			Long dayToBuy = 0l;
-			for (Long l = 0l; l < prices.length; l++) {
-				Long curPrice = prices[l.intValue()];
+			long dayToBuy = 0L;
+			for (long l = 0L; l < prices.length; l++) {
+				Long curPrice = prices[(int) l];
 				if (curPrice < price) {
 					price = curPrice;
 					dayToBuy = l + 1;
 				}
 			}
 			System.out.println("price is " + price + ", dayToBuy is " + dayToBuy);
+
+			// 2. Find best profit.
+			long bestProfit = price;
+			if ((int) dayToBuy == prices.length) {
+				bestProfit = 0L;
+			}
+			for (long i = dayToBuy; i < prices.length; i++) {
+				Long curPrice = prices[(int) i];
+
+				long curProfit = curPrice - price;
+				if (curProfit > bestProfit) {
+					bestProfit = curProfit;
+				}
+			}
+			System.out.println("best profit is " + bestProfit);
 
 		} catch (Exception e) {
 			e.printStackTrace();
