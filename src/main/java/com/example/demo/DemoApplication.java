d4e79097b36542b50bfbeb064063a4e3f79d5dbe
Watchakorn Wongtibet <w.wongtibet@gmail.com>
2024-03-11 21:14:10 +0700

    init q2 and validation input

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,53 +2,79 @@ package com.example.demo;
 
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		DemoApplication demo = new DemoApplication();
-//		 Long[] req = { 7L, 1L, 5L, 3L, 6L, 4L };
-		Long[] req = { 7L, 6L, 5L, 4L, 3L, 1L };
-		demo.q1(req);
-	}
+    public static void main(String[] args) {
+        DemoApplication demo = new DemoApplication();
+        int[] req1 = {7, 1, 5, 3, 6, 4};
+        demo.q1(req1);
+        int[] req2 = {7, 6, 5, 4, 3, 1};
+        demo.q1(req2);
 
-	public void q1(Long[] prices) {
-		System.out.println("q1 begin");
-		try {
-			if (prices == null) {
-				System.out.println("input is null.");
-				return;
-			}
-			System.out.println("Price quantity: " + prices.length);
+        demo.q2("1.01", "1.001");
+        demo.q2("1.0", "1.0.0");
+        demo.q2("0.1", "1.1");
+    }
 
-			// 1. find day to buy and price.
-			Long price = prices[0];
-			long dayToBuy = 0L;
-			for (long l = 0L; l < prices.length; l++) {
-				Long curPrice = prices[(int) l];
-				if (curPrice < price) {
-					price = curPrice;
-					dayToBuy = l + 1;
-				}
-			}
-			System.out.println("price is " + price + ", dayToBuy is " + dayToBuy);
+    public void q1(int[] prices) {
+        System.out.println("q1 begin");
+        try {
+            if (prices == null) {
+                System.out.println("input is null.");
+                return;
+            }
+            System.out.println("Price quantity: " + prices.length);
 
-			// 2. Find best profit.
-			long bestProfit = price;
-			if ((int) dayToBuy == prices.length) {
-				bestProfit = 0L;
-			}
-			for (long i = dayToBuy; i < prices.length; i++) {
-				Long curPrice = prices[(int) i];
+            // 1. find day to buy and price.
+            int price = prices[0];
+            int dayToBuy = 0;
+            for (int l = 0; l < prices.length; l++) {
+                int curPrice = prices[l];
+                if (curPrice < price) {
+                    price = curPrice;
+                    dayToBuy = l + 1;
+                }
+            }
+            System.out.println("price is " + price + ", dayToBuy is " + dayToBuy);
 
-				long curProfit = curPrice - price;
-				if (curProfit > bestProfit) {
-					bestProfit = curProfit;
-				}
-			}
-			System.out.println("best profit is " + bestProfit);
+            // 2. Find best profit.
+            int bestProfit = price;
+            if (dayToBuy == prices.length) {
+                bestProfit = 0;
+            }
+            for (int i = dayToBuy; i < prices.length; i++) {
+                int curPrice = prices[i];
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			System.out.println("q1 end");
-		}
-	}
+                int curProfit = curPrice - price;
+                if (curProfit > bestProfit) {
+                    bestProfit = curProfit;
+                }
+            }
+            System.out.println("best profit is " + bestProfit);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("q1 end");
+        }
+    }
+
+    public void q2(String version1, String version2) {
+        System.out.println("q2 begin");
+        System.out.println("version1 is " + version1 + ", version2 is " + version2);
+        String regex = "^(\\d+)(\\.(\\d+)(\\.(\\d+))?)?(\\-(\\w+)(-(\\w+))?)?$";
+        if (version1 == null || version2 == null) {
+            System.out.println("version1 or version2 is null.");
+            return;
+        }
+        if (version1.length() > 500 || version2.length() > 500) {
+            System.out.println("version1 or version2 has length more than 500.");
+            return;
+        }
+        if (!version1.matches(regex) || !version2.matches(regex)) {
+            System.out.println("version1 or version2 is not a valid version number");
+            return;
+        }
+
+        System.out.println("q2 end");
+    }
+
 }
